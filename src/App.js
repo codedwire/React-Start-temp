cf63e3da68d965cfbe74814aded73644d023456c
Deepak Kumar <structlooper@gmail.com>
2023-02-25 11:00:14 +0530

    boot stata

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import './App.css';
 import React from "react";
 import BootApp from "./views/index";
+import Config from './config'
+import _ from "lodash";
 function App() {
   return (
-    <>
-      <BootApp />
-    </>
+      <>
+        {(Config.env === 'DEV' || Config.env === 'PROD') && <BootApp />}
+        {_.isUndefined(Config.env) && <h1>ENV IS NOT SETUP, CONTACT ADMIN</h1>}
+      </>
   );
 }
 export default App;
