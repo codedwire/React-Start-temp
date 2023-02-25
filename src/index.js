370323704940751956e139d5598654ead541f0c6
Deepak Kumar <structlooper@gmail.com>
2023-02-25 15:22:47 +0530

    structure final with redux

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import {Provider} from "react-redux";
-import {persistVar, store} from "./Redux/store";
+import {persistVar, store} from "./redux/store";
 import { PersistGate } from 'redux-persist/integration/react';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
--- src/views/index.js
+++ src/views/index.js
@@ -5,10 +5,13 @@
 * */
 import React from 'react';
 import {Helmet} from 'react-helmet';
-import {getStateFromSelector} from "../Redux/store";
-import {CURRENT_SESSION_STATE_REDUCER} from "../Redux/reducers";
+import {getStateFromSelector} from "../redux/store";
+import {CURRENT_SESSION_STATE_REDUCER} from "../redux/reducers";
+// import {setCurrentPageTitle} from "../redux/action";
+// import {useDispatch} from "react-redux";
 
 const Application = () => {
+    // const ds = useDispatch()
     const {pageTitle, pageName} = getStateFromSelector(CURRENT_SESSION_STATE_REDUCER);
     return (
         <>
@@ -16,6 +19,9 @@ const Application = () => {
                 <title>{pageTitle}</title>
             </Helmet>
             <h1>This is {pageName}</h1>
+            {/*<button title={"Click me"} onClick={()=>ds(setCurrentPageTitle("CHKCINF"))}>*/}
+            {/*    <h4>Click me</h4>*/}
+            {/*</button>*/}
         </>
     );
 };
